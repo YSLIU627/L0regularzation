0a7bb217e1efc9d744223910c852b0f99e9e32b1
ZH <64350615+YSLIU627@users.noreply.github.com>
2024-05-10 23:03:44 -0500

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ..
 python3 -m L0regularzation.sam_jax.train --dataset cifar10 --model_name WideResnet28x10 \
 --output_dir /tmp/my_experiment --image_level_augmentations autoaugment \
 --num_epochs 1800 --sam_rho 0.05
