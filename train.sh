89185bebe6f0f9f3129c4d514ddbbc9dead122e1
ZH <64350615+YSLIU627@users.noreply.github.com>
2024-05-10 22:56:39 -0500

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python3 -m sam_jax.train --dataset cifar10 --model_name WideResnet28x10 \
+--output_dir /tmp/my_experiment --image_level_augmentations autoaugment \
+--num_epochs 1800 --sam_rho 0.05
