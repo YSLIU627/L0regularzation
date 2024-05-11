86893e2f3a5263b8597758fa01390a28784bf14c
ZH <64350615+YSLIU627@users.noreply.github.com>
2024-05-11 16:08:09 -0500

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python example_cifar.py --dataset CIFAR10 --minimizer SAM --rho 0.05 --optimizer Adam --weight_decay 0
+export CUDA_VISIBLE_DEVICES=1
+#python example_cifar.py --dataset CIFAR10 --minimizer SAM --rho 0.05 --optimizer Adam --weight_decay 0
+python example_cifar.py --dataset CIFAR10 --minimizer no_SAM --rho 0.05 --optimizer Adam --weight_decay 0
